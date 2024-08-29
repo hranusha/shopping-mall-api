b1b44058c332623587e1ed6a9e80a19f9337c752
Hranush Azibekyan <hranush.azibekyan01@gmail.com>
2024-08-30 03:14:45 +0400

    add pagination for products list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,16 +8,44 @@ use App\Rules\ValidSku;
 
 class ProductController extends Controller
 {
-    public function index()
+    /**
+     * Display a paginated list of products.
+     *
+     * @param \Illuminate\Http\Request $request The incoming request containing pagination parameters.
+     * @return \Illuminate\Pagination\LengthAwarePaginator Paginated list of products.
+     */
+    public function index(Request $request)
     {
-        return Product::with('category')->get();
+        $request->validate([
+            'page' => 'integer|min:1|max:100',
+            'per_page' => 'integer|min:1|max:100',
+        ]);
+        
+        $page = $request->input('page', 1);
+        $perPage = $request->input('per_page', 10);
+
+        $products = Product::paginate($perPage, ['*'], 'page', $page);
+
+        return $products;
     }
 
+    /**
+     * Display the specified product along with its category.
+     *
+     * @param int $id The ID of the product to retrieve.
+     * @return \Illuminate\Database\Eloquent\Model The product with its associated category.
+     */
     public function show($id)
     {
         return Product::with('category')->findOrFail($id);
     }
 
+    /**
+     * Store a newly created product in the database.
+     *
+     * @param \Illuminate\Http\Request $request The incoming request containing product data.
+     * @return \Illuminate\Http\JsonResponse The created product in JSON format.
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -33,6 +61,13 @@ class ProductController extends Controller
         return response()->json($product, 201);
     }
 
+    /**
+     * Update the specified product in the database.
+     *
+     * @param \Illuminate\Http\Request $request The incoming request containing updated product data.
+     * @param int $id The ID of the product to update.
+     * @return \Illuminate\Http\JsonResponse The updated product in JSON format.
+     */
     public function update(Request $request, $id)
     {
         $product = Product::findOrFail($id);
@@ -50,6 +85,12 @@ class ProductController extends Controller
         return response()->json($product, 200);
     }
 
+    /**
+     * Remove the specified product from the database.
+     *
+     * @param int $id The ID of the product to delete.
+     * @return \Illuminate\Http\JsonResponse An empty response with a 204 status code.
+     */
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
